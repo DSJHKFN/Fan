f63d911cb103be00dc0d81c8f6ba187fc550c2ae
DSJHKFN <99450247+DSJHKFN@users.noreply.github.com>
2023-09-17 00:54:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function hi(){
 var x = document.getElementById("3").value;
-console.log(x);
-document.getElementById("4").innerHTML = x;
+y = console.log(x);
+document.getElementById("4").innerHTML = y;
 }
